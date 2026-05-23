9a8bfd10d6c2f5d62532c0221e1eb76fd438db25
Tu Nguyen <minhtu1392000@gmail.com>
2026-05-23 14:00:11 +0700

    ui: add stock shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, Roboto } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
 
 import type { Metadata } from 'next';
+
 import './globals.css';
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,11 +31,14 @@ export default function RootLayout({
   return (
     <html
       lang='en'
-      className={`
-        ${geistSans.variable}
-        ${geistMono.variable}
-        h-full antialiased
-      `}
+      className={cn(
+        'h-full',
+        'antialiased',
+        geistSans.variable,
+        geistMono.variable,
+        'font-sans',
+        roboto.variable,
+      )}
     >
       <body className='flex min-h-full flex-col'>{children}</body>
     </html>
