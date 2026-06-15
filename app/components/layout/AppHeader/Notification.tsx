3b7185b1217e81fb224146d5dd4cb4aac11a9bbb
Tu Nguyen <minhtu1392000@gmail.com>
2026-06-15 18:51:44 +0700

    impr(layout/notification): expand to dropdown with empty state

--- app/components/layout/AppHeader/Notification.tsx
+++ app/components/layout/AppHeader/Notification.tsx
@@ -1,11 +1,43 @@
-import { IconBell } from '@tabler/icons-react';
+'use client';
 
+import { IconBell } from '@tabler/icons-react';
+import { useTranslations } from 'next-intl';
+
+import { SimpleEmpty } from '../../simple/SimpleEmpty';
 import { Button } from '../../ui/Button';
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent } from '../../ui/DropdownMenu';
+import { EmptyMedia } from '../../ui/Empty';
+
+function NotificationEmpty() {
+  const t = useTranslations('components.notification.empty');
+
+  return (
+    <SimpleEmpty
+      title={t('title')}
+      description={t('description')}
+      media={
+        <EmptyMedia variant='icon'>
+          <IconBell />
+        </EmptyMedia>
+      }
+    />
+  );
+}
 
 export function NotificationButton() {
   return (
-    <Button size='icon' variant='outline'>
-      <IconBell />
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        render={
+          <Button size='icon' variant='outline'>
+            <IconBell />
+          </Button>
+        }
+      />
+
+      <DropdownMenuContent className='w-80' side='bottom' align='end' sideOffset={4}>
+        <NotificationEmpty />
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
