2477d8a0e21cdca3be5acccc3d2bc6b1592cfc88
Tu Nguyen <minhtu1392000@gmail.com>
2026-06-16 12:42:45 +0700

    chore(fe): prevent accessing dummy landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
-import { getTranslations } from 'next-intl/server';
+import { redirect } from 'next/navigation';
 
 import { getUser } from '@/app/lib/better-auth/session';
 
-import { Link } from './components/ui/Link';
-import { H2 } from './components/ui/Typography';
-
 export default async function LandingPage() {
-  const t = await getTranslations('landingPage');
   const user = await getUser();
 
-  return (
-    <div>
-      <H2>Landing Page</H2>
-      {user ? (
-        <Link href='/dashboard'>{t('toDashboard')}</Link>
-      ) : (
-        <Link href='/sign-in'>{t('toSignIn')}</Link>
-      )}
-    </div>
-  );
+  if (user) {
+    redirect('/dashboard');
+  } else {
+    redirect('/sign-in');
+  }
 }
